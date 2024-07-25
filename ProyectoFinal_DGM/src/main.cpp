7be10af73c300c4a12e6fa0da9cf24d545a74bb2
PoLeNQuE <martniciop@gmail.com>
2024-07-25 17:42:17 -0300

    cambios

--- ProyectoFinal_DGM/src/main.cpp
+++ ProyectoFinal_DGM/src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
+#include "SensorDHT11.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHTPIN 13
+
+SensorDHT11 sensor(DHTPIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    sensor.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    float temperatura = sensor.readTemperature();
+    float humedad = sensor.readHumidity();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.print("Temperatura: ");
+    Serial.print(temperatura);
+    Serial.println(" *C");
+
+    Serial.print("Humedad: ");
+    Serial.print(humedad);
+    Serial.println(" %");
+
+    delay(2000);
 }
